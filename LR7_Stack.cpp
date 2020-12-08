2b0394ee60f0a80619e477f1237092f1b0123a93
Ivan <70635146+themilchenko@users.noreply.github.com>
2020-12-08 16:27:23 +0300

    Update LR7_Stack.cpp

--- LR7_Stack.cpp
+++ LR7_Stack.cpp
@@ -4,87 +4,103 @@
 
 struct Node
 {
-	Node* previous = nullptr;
-	int information;
+    Node* previous = nullptr;
+    int information = 0;
 };
 
 struct Stack
 {
-	Node* head;
-	Node* tail;
+    Node* head;
+    Node* tail;
 };
 
 void Constructor(Stack& stack)
 {
-	stack.head = nullptr;
-	stack.tail = nullptr;
+    stack.head = nullptr;
+    stack.tail = nullptr;
 }
 
 void push(Stack& stack, Node& node)
 {
-	if (stack.head == nullptr)
-	{
-		Node* new_element = new Node;
-		new_element->information = node.information;
-		stack.head = new_element;
-		stack.tail = new_element;
-	}
-	else
-	{
-		Node* new_element = new Node;
-		new_element->information = node.information;
-		new_element->previous = stack.tail;
-		stack.tail = new_element;
-	}
+    if (stack.head == nullptr)
+    {
+        Node* new_element = new Node;
+        new_element->information = node.information;
+        stack.head = new_element;
+        stack.tail = new_element;
+    }
+    else
+    {
+        Node* new_element = new Node;
+        new_element->information = node.information;
+        new_element->previous = stack.tail;
+        stack.tail = new_element;
+    }
 }
 
 Node& pop(Stack& stack)
 {
-	Node* rezulting_value = new Node;
-	Node* node = stack.tail;
-	rezulting_value->information = node->information;
-	stack.tail = stack.tail->previous;
-	delete node;
-	return*rezulting_value;
+    Node* rezulting_value = new Node;
+    Node* node = stack.tail;
+    rezulting_value->information = node->information;
+    stack.tail = stack.tail->previous;
+    delete node;
+    return*rezulting_value;
 }
 
 unsigned int size(const Stack& stack)
 {
-	int counter = 0;
-	Node* node = stack.tail;
-	while (node != stack.tail)
-	{
-		node = node->previous;
-		counter++;
-	}
-	return counter;
+    int counter = 0;
+    Node* node = stack.tail;
+    while (node != stack.head)
+    {
+        node = node->previous;
+        counter++;
+    }
+    counter++;
+    return counter;
 }
 
 void destructor(Stack& stack)
 {
-	while (stack.tail != nullptr)
-	{
-		Node* used = new Node;
-		used = stack.tail;
-		stack.tail = stack.tail->previous;
-		delete used;
-	}
+    while (stack.tail != nullptr)
+    {
+        Node* used = new Node;
+        used = stack.tail;
+        stack.tail = stack.tail->previous;
+        delete used;
+    }
+}
+
+void print(const Stack& stack)    // Print elements.
+{
+    for (auto i = stack.tail; i != nullptr; i = i->previous)
+        std::cout << i->information << ' ';
 }
 
 int main()
 {
-	Stack stack;
-	Constructor(stack);
-	for (int i = 0; i < 7; i++)
-	{
-		Node node;
-		node.information = i;
-		push(stack, node);
-	}
-	std::cout << "There are " << size(stack) << " elements in queue" << std::endl;
-	Node node = pop(stack);
-	std::cout << "There are " << node.information << " popped elements in queue" << std::endl;
-	destructor(stack);
-	std::cout << "There are " << size(stack) << " elements in queue" << std::endl;
-	return 0;
+    Stack stack;
+
+    Constructor(stack);
+
+    for (int i = 10; i > 0; i--)
+    {
+        Node node;
+        node.information = i;
+        push(stack, node);
+    }
+
+    print(stack);
+
+    std::cout << std::endl << "The size of stack: " << size(stack);
+
+    Node node = pop(stack);
+    std::cout << std::endl << "There are " << node.information
+    << " popped elements in queue" << std::endl;
+
+    destructor(stack);
+
+    std::cout << "There are " << size(stack) << " elements in queue" << std::endl;
+    return 0;
 }
